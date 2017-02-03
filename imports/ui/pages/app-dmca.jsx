a354243645dd9289df0e5a58e22d866925ed0cd9
Dan Balan <dan.balan0712@gmail.com>
2017-02-03 02:30:37 +0200

    create dmca

---
+++ imports/ui/pages/app-dmca.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+
+const AppDmca = () => (
+  <div>
+    <h2>DMCA</h2>
+    <h4>Mostmp3.com - Digital Millennium Copyright Act ("DMCA") Policy </h4>
+		<section>
+		 	<label>
+			  Introduction
+			</label>
+			<p>
+			  This policy is intended to implement the procedures set forth in 17 U.S.C. Section 512 and the Digital Millennium Copyright Act ("DMCA") for the reporting of alleged copyright infringement. It is the policy of Mostmp3.com services (the "Company") to respect the legitimate rights of copyright owners, their agents, and representatives. Users of any part of the Company computing system are required to respect the legal protections provided by applicable copyright law.
+			</p>
+			<label>
+			  Designated Agent
+			</label>
+			<p>
+			  To exercise your DMCA rights, you must send your Proper DMCA Notice to the following agent designated by Company (the "Designated Agent"). The contact information for Mostmp3.com (previously Spotifytube.com) Designated Agent is:
+				Email: infospotifytube@gmail.com
+				This email address may be used only for copyright infringement claims - you will not receive a reply if the matter is not a copyright issue.
+				Upon receipt of proper notification of claimed infringement, Company will follow the procedures outlined herein and in the DMCA.
+			</p>
+			<label>
+			  Complaint Notice Procedures for Copyright Owners
+			</label>
+			<p>
+			  The following elements must be included in your copyright infringement claim:
+			</p>
+			<ul>
+		    <li>An electronic or physical 
+		      signature of the copyright owner or a person authorized to act on behalf 
+		      of the owner of an exclusive right that is allegedly infringed. </li>
+		    <li>Identification of the copyrighted 
+		      work claimed to have been infringed, or if multiple copyrighted works 
+		      at a single online site are covered by a single notice, a representative 
+		      list of such works at that site. </li>
+		    <li>Identification of the material 
+		      that is claimed to be infringing or to be the subject of infringing 
+		      activity and that is to be removed or access to which is to be disabled, 
+		      and information reasonably sufficient to permit the Company to locate 
+		      the material. </li>
+		    <li>Information reasonably sufficient 
+		      to permit the Company to contact the complaining party, such as an address, 
+		      telephone number, and, if available, an electronic mail address at which 
+		      the complaining party may be contacted. </li>
+		    <li>A statement that the complaining 
+		      party has a good faith belief that use of the material in the manner 
+		      complained of is not authorized by the copyright owner, its agent, or 
+		      the law. </li>
+		    <li>A statement that the information 
+		      in the notice is accurate, and under penalty of perjury, that the complaining 
+		      party is authorized to act on behalf of the owner of an exclusive right 
+		      that is allegedly infringed. </li>
+		  </ul>
+		  <p>
+		    Failure to include all of the above information may result in a delay of the processing or the DCMA notification.
+		  </p>
+		  <label>
+		    Notice and Takedown Procedure
+		  </label>
+		  <p>
+		    It is expected that all users of any part of the Company system will comply with applicable copyright laws. However, if the Company receives proper notification of claimed copyright infringement it will respond expeditiously by removing, or disabling access to, the material that is claimed to be infringing or to be the subject of infringing activity provided all such claims have been investigated and determined to be valid by Company in Company's sole and absolute discretion. Company will comply with the appropriate provisions of the DMCA in the event a counter notification is received.
+		  </p>
+		  <p>
+		    Please allow at least three business days to process any complaint or counter-notification. Note that emailing your complaint to other parties such as our Internet Service Provider will not expedite your request and may result in a delayed response due to the complaint not being filed properly.
+		  </p>
+		  <label>
+		    Repeat Infringers
+		  </label>
+		  <p>
+		    Under appropriate circumstances, Company may, in its discretion, terminate authorization of users of its system or network who are repeat infringers.
+		  </p>
+		  <label>
+		    Accommodation of Standard Technical Measures
+		  </label>
+		  <p>
+		    It is Company policy to accommodate and not interfere with standard technical measures it determines are reasonable under the circumstances, i.e., technical measures that are used by copyright owners to identify or protect copyrighted works.
+		  </p>
+		</section>
+  </div>
+);
+
+export default AppDmca;
