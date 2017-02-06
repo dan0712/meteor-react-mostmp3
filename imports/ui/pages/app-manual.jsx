6a9258a328089f8264da789d8b47658c59e4d606
Dan Balan <dan.balan0712@gmail.com>
2017-02-06 13:51:12 +0200

    create app manual

---
+++ imports/ui/pages/app-manual.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+
+const AppManual = () => (
+  <div>
+    <h1>How to download Spotify to mp3?</h1>
+    <section className="manual">
+	    <h4>
+			  <label>
+		      How to download Spotify playlist in mp3?
+	      </label>
+	    </h4>
+			<div className="row">
+				<div className="col-md-6">
+					<ol>
+						<li>
+							<p>
+							Choose any playlist you like and save the playlist to your collection in Spotify app
+							</p>
+						</li>
+						<li>
+							<p>
+							Login with your Spotify account and go to your Playlists
+							</p>
+						</li>
+						<li>
+							<p>
+							Click download button next to the Spotify track you want to download in mp3
+							</p>
+						</li>
+						<li>
+							<p>
+							Note that Mostmp3 currently only searches for songs on Youtube. If your desired song is not on Youtube, it won't find it.
+							</p>
+						</li>
+						<li>
+							<p>
+							Wait until mp3 will be downloaded
+							</p>
+						</li>
+						<li>
+							<p>
+							Play in your favorite player. If it doesn't work, use <a rel="nofollow" href="http://www.videolan.org/vlc/index.html">VLC player</a>
+							</p>
+						</li>
+					</ol>
+				</div>
+				<div className="col-md-6">
+					<iframe src="//www.youtube.com/embed/_Xyv08i87hQ?showinfo=0&amp;rel=0&amp;autohide=1&amp;theme=light"></iframe>
+				</div>
+			</div>
+		</section>
+		<section className="manual">
+		  <h4>
+			  <label>
+					How to download Spotify songs in mp3?
+				</label>
+			</h4>
+			<div className="row">
+				<div className="col-md-6">
+					<ol>
+						<li>
+							<p>
+							Login with your Spotify account
+							</p>
+						</li>
+						<li>
+							<p>
+							Choose any track you like or find tracks by searching for a term
+							</p>
+						</li>
+						<li>
+							<p>
+							Click download button next to the Spotify track you want to download in MP3
+							</p>
+						</li>
+						<li>
+							<p>
+							Note that Mostmp3 currently only searches for songs on Youtube. If your desired song is not on Youtube, it won't find it.
+							</p>
+						</li>
+						<li>
+							<p>
+							Wait until MP3 will be downloaded
+							</p>
+						</li>
+						<li>
+							<p>
+							Play in your favorite player. If it doesn't work, use <a rel="nofollow" href="http://www.videolan.org/vlc/index.html">VLC player</a>
+							</p>
+						</li>
+					</ol>
+				</div>
+				<div className="col-md-6">
+					<iframe src="//www.youtube.com/embed/asNEyzLPj9c?showinfo=0&amp;rel=0&amp;autohide=1&amp;theme=light"></iframe>
+				</div>
+			</div>
+		</section>
+  </div>
+);
+
+export default AppManual;
